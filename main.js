65faeb85c5d2a0a333a2c0c78fc25a86f5082ef0
lucamagenes <lucamgns33@gmail.com>
2021-10-25 12:21:44 +0200

    commit squadre di calcio

---
+++ main.js
@@ -0,0 +1,90 @@
+//Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+/* const biciclette = [
+    {
+        nome: 'bici da corsa 1',
+        peso: '8'
+    },
+    {
+        nome: 'bici da corsa 2',
+        peso: '1'
+    },
+    {
+        nome: 'bici da corsa 3',
+        peso: '4'
+    },
+    {
+        nome: 'bici da corsa 4',
+        peso: '7'
+    },
+    {
+        nome: 'bici da corsa 5',
+        peso: '9'
+    },
+    {
+        nome: 'bici da corsa 6',
+        peso: '3'
+    },
+    {
+        nome: 'bici da corsa 7',
+        peso: '5'
+    },
+    {
+        nome: 'bici da corsa 8',
+        peso: '2'
+    },
+] */
+
+//const pesoBici = biciclette[''].peso;
+
+/* let biciLeggera = biciclette[0];
+
+for (let index = 0; index < biciclette.length; index++) {
+    const bici = biciclette[index];
+    console.log(bici.peso);
+
+    if (bici.peso < biciLeggera.peso) {
+        biciLeggera = bici;
+
+    }
+
+}
+
+console.log(biciLeggera);
+
+document.querySelector('.risultato').innerHTML = `La bici più leggera è : ${biciLeggera.nome} e pesa: ${biciLeggera.peso} ` */
+
+
+//__________________________________________
+
+
+//Creare un array di oggetti di squadre di calcio.
+
+//Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+
+//Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0. 
+
+
+const squadreCalcio = [
+    {
+        nome: 'milan',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'juventus',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'inter',
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'roma',
+        punti: 0,
+        falliSubiti: 0,
+    },
+]
